14e9a84437811482703d44b1f86265700ca3e236
Davinir Fabretti de Campos Jr <davinir.jr@gmail.com>
2020-12-03 15:25:09 -0300

    fix(resource): Added a real public SSH key in main.tf

--- examples/ssh_key/main.tf
+++ examples/ssh_key/main.tf
@@ -8,8 +8,8 @@ terraform {
 }
 
 resource "hivelocity_ssh_key" "demo" {
-  name = "My Terraform SSH Key"
-  public_key = "My Terraform SSH Public Key"
+  name = "This is my Terraform SSH Key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDYYGqXPIJ1yQ7pMTSmdB+LrjvnPh2jSJewvinxYwuk/CBvbsUgFxOTBtjzfU0kHIlo5QyIhJ7tjh8PhT8LdJPGB86ItwTg3Lmt1q5UFxbHmZ0kPmmzaDI/9aakOal3P93D14HDCzBnkTHfC8/JZ5JpDxp86XM+gWQ9sFMkLx83ZOwONNu3E8PowTEpsp0jMx2B2aFeSM+T4bLkJJQtA5Cp6lgRAc5AklXaqmpdAil/fIL/+gvRf8kJIccAe/2oVj4flaMK7mgZ39qhzYYUjTEokEYvJf17QbdtFTxxtIQ+hTxzKzwT6p8cMu6DNQLfq6oxzbuBVGTvKD79MR5vjx+RNpaIru8wzIspHTez9eGDzdR0316GWDcxmMwVIDMM+3pjopDJV6DILfs6hVlAuH11yCX8YwwGHYpsdzLLd00FEEaGLGVRDr/hvduZ1caQIvdNln6Gr7k6W51U1VTC3NRq49yoxYSsXxn30PfTe2IxFaZyhQXHunCLaMCF+TrAOc0= someone@somewhere.com"
 }
 
 output "demo_device" {
