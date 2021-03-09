a18123bbeaf55e482bdf0e0600137b0a1153b351
rafikrafael <rafikrafael@gmail.com>
2021-03-09 17:35:48 -0300

    fix(product): added an error msg when no product is found

--- hivelocity/data_source_product.go
+++ hivelocity/data_source_product.go
@@ -73,7 +73,14 @@ func dataSourceProductRead(ctx context.Context, d *schema.ResourceData, m interf
 	}
 
 	if product == nil {
-		return nil
+		var diags diag.Diagnostics
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "Product not found.",
+			Detail:   "No products matched the search.",
+		})
+
+		return diags
 	}
 
 	for k, v := range product {
