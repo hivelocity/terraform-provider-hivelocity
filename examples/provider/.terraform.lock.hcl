9d63b2a2096b6f27fdee4d8862a3e45548357937
Juan <jpqokubo@gmail.com>
2023-08-21 12:45:06 -0400

    sp-299 fix source product

---
+++ examples/provider/.terraform.lock.hcl
@@ -0,0 +1,9 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hivelocity/hivelocity" {
+  version = "0.1.0"
+  hashes = [
+    "h1:30ngyMbpRw3s3mF4C4K2LT9o0EOW8m0UJesYSozAgfo=",
+  ]
+}
--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,37 +1,37 @@
 terraform {
   required_providers {
     hivelocity = {
-      version  = "0.5.0"
       source   = "hivelocity/hivelocity"
     }
   }
 }
-
+provider "hivelocity" {
+  api_key="d13344dc3b2b48f6e036caefa47f45f4ed9991ea"
+}
 // Find a plan with 16GB of memory in Tampa.
 data "hivelocity_product" "tampa_product" {
   first = true
 
-  filter {
-    name   = "product_memory"
-    values = ["16GB"]
-  }
-
-  filter {
-    name   = "data_center"
-    values = ["TPA1"]
-  }
 
   filter {
     name   = "stock"
     values = ["limited", "available"]
   }
+
+  # filter {
+  #   name   = "stock"
+  #   values = ["limited", "available"]
+  # }
+
+
 }
 
+
 // Create a SSH Key
-resource "hivelocity_ssh_key" "my_ssh_key" {
-  name       = "This is my Terraform SSH Key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDYYGqXPIJ1yQ7pMTSmdB+LrjvnPh2jSJewvinxYwuk/CBvbsUgFxOTBtjzfU0kHIlo5QyIhJ7tjh8PhT8LdJPGB86ItwTg3Lmt1q5UFxbHmZ0kPmmzaDI/9aakOal3P93D14HDCzBnkTHfC8/JZ5JpDxp86XM+gWQ9sFMkLx83ZOwONNu3E8PowTEpsp0jMx2B2aFeSM+T4bLkJJQtA5Cp6lgRAc5AklXaqmpdAil/fIL/+gvRf8kJIccAe/2oVj4flaMK7mgZ39qhzYYUjTEokEYvJf17QbdtFTxxtIQ+hTxzKzwT6p8cMu6DNQLfq6oxzbuBVGTvKD79MR5vjx+RNpaIru8wzIspHTez9eGDzdR0316GWDcxmMwVIDMM+3pjopDJV6DILfs6hVlAuH11yCX8YwwGHYpsdzLLd00FEEaGLGVRDr/hvduZ1caQIvdNln6Gr7k6W51U1VTC3NRq49yoxYSsXxn30PfTe2IxFaZyhQXHunCLaMCF+TrAOc0= someone@somewhere.com"
-}
+# resource "hivelocity_ssh_key" "my_ssh_key" {
+#   name       = "This is my Terraform SSH Key"
+#   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDYYGqXPIJ1yQ7pMTSmdB+LrjvnPh2jSJewvinxYwuk/CBvbsUgFxOTBtjzfU0kHIlo5QyIhJ7tjh8PhT8LdJPGB86ItwTg3Lmt1q5UFxbHmZ0kPmmzaDI/9aakOal3P93D14HDCzBnkTHfC8/JZ5JpDxp86XM+gWQ9sFMkLx83ZOwONNu3E8PowTEpsp0jMx2B2aFeSM+T4bLkJJQtA5Cp6lgRAc5AklXaqmpdAil/fIL/+gvRf8kJIccAe/2oVj4flaMK7mgZ39qhzYYUjTEokEYvJf17QbdtFTxxtIQ+hTxzKzwT6p8cMu6DNQLfq6oxzbuBVGTvKD79MR5vjx+RNpaIru8wzIspHTez9eGDzdR0316GWDcxmMwVIDMM+3pjopDJV6DILfs6hVlAuH11yCX8YwwGHYpsdzLLd00FEEaGLGVRDr/hvduZ1caQIvdNln6Gr7k6W51U1VTC3NRq49yoxYSsXxn30PfTe2IxFaZyhQXHunCLaMCF+TrAOc0= someone@somewhere.com"
+# }
 
 // Provision your devices with CentOS 7.
 resource "hivelocity_bare_metal_device" "webserver" {
@@ -40,24 +40,22 @@ resource "hivelocity_bare_metal_device" "webserver" {
   location_name     = data.hivelocity_product.tampa_product.data_center
   hostname          = "webserver.terraform.test"
   tags              = ["hello", "world"]
-  script            = file("${path.module}/cloud_init_example.yaml")
-  public_ssh_key_id = hivelocity_ssh_key.my_ssh_key.ssh_key_id
 }
 
-resource "hivelocity_bare_metal_device" "database" {
-  product_id        = data.hivelocity_product.tampa_product.product_id
-  os_name           = "CentOS 7.x"
-  location_name     = data.hivelocity_product.tampa_product.data_center
-  hostname          = "database.terraform.test"
-  tags              = ["hello", "world"]
-  script            = file("${path.module}/cloud_init_example.yaml")
-  public_ssh_key_id = hivelocity_ssh_key.my_ssh_key.ssh_key_id
-}
+# resource "hivelocity_bare_metal_device" "database" {
+#   product_id        = data.hivelocity_product.tampa_product.product_id
+#   os_name           = "CentOS 7.x"
+#   location_name     = data.hivelocity_product.tampa_product.data_center
+#   hostname          = "database.terraform.test"
+#   tags              = ["hello", "world"]
+#   script            = file("${path.module}/cloud_init_example.yaml")
+#   public_ssh_key_id = hivelocity_ssh_key.my_ssh_key.ssh_key_id
+# }
 
 // Create a VLAN connecting servers
-resource "hivelocity_vlan" "private_vlan" {
-  device_ids    = [
-      hivelocity_bare_metal_device.webserver.device_id,
-      hivelocity_bare_metal_device.database.device_id,
-  ]
-}
+# resource "hivelocity_vlan" "private_vlan" {
+#   device_ids    = [
+#       hivelocity_bare_metal_device.webserver.device_id,
+#       hivelocity_bare_metal_device.database.device_id,
+#   ]
+# }
